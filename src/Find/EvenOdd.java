ee35505300fb60d26ac8ba67e3e746eadc009a21
abhi <abhilashnaidu21@gmail.com>
2024-01-19 16:32:11 +0530

    new

---
+++ src/Find/EvenOdd.java
@@ -0,0 +1,22 @@
+package Find;
+
+import java.util.Scanner;
+
+public class EvenOdd{
+
+	public static void main(String[] args)throws Exception {
+
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter value");
+	int num = sc.nextInt();
+	
+	if(num%2==0) {
+		System.out.println("even number");
+	}else {
+		System.out.println("odd number");
+	}
+	
+		
+		
+	}
+}
