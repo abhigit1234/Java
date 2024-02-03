9e9170832b641cec6b8b7896dbbe040e0ff42a94
abhi <abhilashnaidu21@gmail.com>
2024-02-03 22:14:01 +0530

    new

--- src/Find/PrimeNumber.java
+++ src/Find/PrimeNumber.java
@@ -5,21 +5,8 @@ import java.util.Scanner;
 public class PrimeNumber {
 	public static void main(String[] args) {
 		
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter valiue");
-		int num = sc.nextInt();
-		int count=0;
-		for (int i = 1; i <= num; i++) {
-			if(num%i==0) {
-				count++;
-			}
-		}
-		if(count==2) {
-			System.out.println("Prime num");
-		}else {
-			System.out.println("Not a Prime num");
-		}
-		
+		int num =7;
+		System.out.println(7%5);
 		
 	}
 }
