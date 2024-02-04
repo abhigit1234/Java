797f3c8d1120cef6fc9cccc875ce05e6ad4ce6a7
abhi <abhilashnaidu21@gmail.com>
2024-02-04 11:25:43 +0530

    new

--- src/Find/PrimeNumber.java
+++ src/Find/PrimeNumber.java
@@ -2,19 +2,21 @@ package Find;
 
 public class PrimeNumber {
 	public static void main(String[] args) {
-
-		int num = 12;
-		int count = 0;
+		int num =41;
+		int count=0;
 		for (int i = 1; i <= num; i++) {
-			if (num % i == 0) {
+			if(num%i==0) {
 				count++;
 			}
+		}if(count==2) {
+			System.out.println("P");
+		}else {
+			System.out.println("N");
 		}
-		if (count == 2) {
-			System.out.println("Prime Num");
-		} else {
-			System.out.println("Not a Prime Num");
-		}
-
+	
+	
+	
+	
+	
 	}
 }
