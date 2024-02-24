850af1ee91b35300d484efb58fd5b9915ad1408e
abhigit1234 <150925884+abhigit1234@users.noreply.github.com>
2024-02-24 18:10:32 +0530

    new file

---
+++ src/Find/PrintNumDivBy10.java
@@ -0,0 +1,15 @@
+package Utilities;
+
+public class PrintNumDivBy10 {
+	public static void main(String[] args) {
+		
+		for(int i=1;i<1000;i++) {
+			if(i%10==0) {
+				System.out.println(i);	
+			}
+		}
+	
+	
+	}
+
+}
