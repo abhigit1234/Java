13fefa16c3910387b140eae6c952f4c0c548be69
abhi <abhilashnaidu21@gmail.com>
2024-02-26 18:02:08 +0530

    nnn

--- src/Find/LargestWord1.java
+++ src/Find/LargestWord1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Find;
 
 public class LargestWord1 {
@@ -18,24 +17,3 @@ public class LargestWord1 {
 	
 	}
 }
-=======
-package Find;
-
-public class LargestWord1 {
-	public static void main(String[] args) throws Exception{
-		
-	String s = "hello  my name is abhilash babu";
-	String[] text =s.split(" ");
-	String temp="";
-	
-	for (int i = 0; i < text.length; i++) {
-		if(text[i].length()>temp.length()) {
-			temp=text[i];
-		}
-		
-	}System.out.println(temp);
-	
-	
-	}
-}
->>>>>>> 42f71c7ea9b5d975f473641acdf2a7246ad5343b
--- src/Find/SplitString.java
+++ src/Find/SplitString.java
@@ -1,34 +1,14 @@
-<<<<<<< HEAD
 package Find;
-
 
 public class SplitString {
 	public static void main(String[] args) {
-		
-		
+
 		String s = "automation is my job";
-		String[]  ss = s.split("\\s");
-		for(String x : ss) {
+		String[] ss = s.split("\\s");
+		for (String x : ss) {
 			System.out.println(x.charAt(1));
 		}
-		
-		
-	}
-=======
-package Find;
 
-
-public class SplitString {
-	public static void main(String[] args) {
-		
-		
-		String s = "automation is my job";
-		String[]  ss = s.split("\\s");
-		for(String x : ss) {
-			System.out.println(x.charAt(1));
-		}
-		
-		
 	}
->>>>>>> 42f71c7ea9b5d975f473641acdf2a7246ad5343b
+
 }
