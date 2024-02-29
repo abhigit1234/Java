410d06f01cba11a108cb354106103bb5e3a34df6
abhigit1234 <150925884+abhigit1234@users.noreply.github.com>
2024-02-29 10:52:18 +0530

    Add files via upload

---
+++ src/Find/ReverseWord.java
@@ -0,0 +1,22 @@
+package Demo;
+
+public class ReverseWord {
+
+	public static void main(String[] args) {
+	
+	
+	
+	
+		String s = "hello abhilash how are you";
+		String[] s1=s.split(" ");
+		int temp=0;
+		StringBuffer sb = new StringBuffer();
+		for (int i = s1.length-1; i>=0;i--) {
+			sb.append(s1[i]).append(" ");
+			
+		}System.out.println(sb);
+	
+	
+	}
+
+}
