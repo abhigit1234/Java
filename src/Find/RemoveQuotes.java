5179221377e4f92db2996a8a04e7791516a35f55
abhi <abhilashnaidu21@gmail.com>
2024-03-28 12:44:40 +0530

    new

---
+++ src/Find/RemoveQuotes.java
@@ -0,0 +1,15 @@
+package Find;
+
+public class RemoveQuotes {
+
+	public static void main(String[] args) {
+
+		String s = "'abhilash'";
+		System.out.println(s.replaceAll("\'", "")); 
+		
+	
+	
+	
+	
+	}
+}
