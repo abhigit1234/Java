56e2ac94980fb218f2659d9ba4a7be8586851be7
abhilash <abhilashnaidu21@gmail.com>
2024-12-23 18:58:25 +0530

    lowercase

---
+++ src/Find/LowerCaseUpperCase.java
@@ -0,0 +1,25 @@
+package Find;
+
+import java.util.Scanner;
+
+public class LowerCaseUpperCase {
+	public static void main(String[] args) throws Exception {
+		/*
+		 * Scanner sc = new Scanner(System.in); System.out.println("enter char"); String
+		 * s1 = sc.nextLine(); String s2 = s1.toUpperCase(); System.out.println(s2);
+		 */
+
+		/*
+		 * String s = "abhilash babu"; String[] s3 = s.split("\\s"); for (String s4 :
+		 * s3) { System.out.println(s4.toUpperCase()); }
+		 */
+	
+		
+		
+	
+	}
+	
+	
+	
+	
+}
